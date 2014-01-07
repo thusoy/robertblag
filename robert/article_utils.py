00a7433c526e82d1bab1632bbfdc1d6b78f6286d
Tarjei Husøy <tarjei@roms.no>
2014-01-07 22:16:05 +0100

    Add a banner to each page
    On the frontpage this is a fixed banner image of bobby, while for the
    article pages this will be picked randomly.

--- robert/article_utils.py
+++ robert/article_utils.py
@@ -2,12 +2,14 @@
     Some tools for fetching articles and their metadata from the repo.
 """
 
-from flask import Markup
+from flask import Markup, url_for
 from os import path, listdir
 from datetime import datetime
 import markdown
+import random
 import re
 import subprocess
+
 
 REPO_ROOT = path.join(path.dirname(__file__), '..')
 ARTICLE_FILE_PATTERN = re.compile(r'.*\.md$')
@@ -22,8 +24,9 @@ def get_articles():
     Articles are sorted by the time added to the repo.
     """
     articles = []
+    bg_imgs = _get_randomized_bg_imgs()
     article_dir = path.join(path.dirname(__file__), '..', 'articles')
-    for article_file in listdir(article_dir):
+    for index, article_file in enumerate(listdir(article_dir)):
         if not ARTICLE_FILE_PATTERN.match(article_file):
             continue
         article_path = path.relpath(path.join(article_dir, article_file))
@@ -37,8 +40,16 @@ def get_articles():
         teaser_breakpoint = rendered_content.index('</p>') + 4
         article['teaser'] = rendered_content[:teaser_breakpoint]
         article['content'] = rendered_content[teaser_breakpoint:]
+        article['bg_img'] = bg_imgs[index]
         articles.append(article)
     return sorted(articles, key=lambda a: a['date_added'], reverse=True)
+
+
+def _get_randomized_bg_imgs():
+    all_imgs = listdir(path.join(path.dirname(__file__), 'static', 'img', 'bgs'))
+    random_imgs = [url_for('static', filename='img/bgs/%s' % img) for img in all_imgs]
+    random.shuffle(random_imgs)
+    return random_imgs
 
 
 def _get_first_commit(article_path):
